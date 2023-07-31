c0cca7db1e32be0cf537f14f139bdeff3c21c3ee
driven <aldazpaulo@gmail.com>
2023-07-31 11:11:11 -0500

    creacion de activity para enviar

---
+++ app/src/main/java/com/aldaz/lws/ui/activities/EnviarActivity.kt
@@ -0,0 +1,13 @@
+package com.aldaz.lws.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aldaz.lws.R
+
+class EnviarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enviar)
+    }
+}
