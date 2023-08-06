dd7a1bffd3e50d406c3911f2c129c512f155756c
driven <aldazpaulo@gmail.com>
2023-08-05 22:25:34 -0500

    creacion de activity para enviar

---
+++ app/src/main/java/com/aldaz/lws/data/dao/lws/LwsChardDAO.kt
@@ -0,0 +1,8 @@
+package com.aldaz.lws.data.dao.lws
+
+import retrofit2.http.Query
+
+interface LwsChardDAO {
+
+//    @Query(select * from LwsChars)
+}
--- app/src/main/java/com/aldaz/lws/logic/lwsLogic/LwsLogic.kt
+++ app/src/main/java/com/aldaz/lws/logic/lwsLogic/LwsLogic.kt
@@ -1,4 +1,12 @@
 package com.aldaz.lws.logic.lwsLogic
 
+import com.aldaz.lws.logic.data.LwsChars
+
 class LwsLogic {
+
+    suspend fun getAll(): List<LwsChars>?{
+        var items : ArrayList<LwsChars> = arrayListOf()
+//        val items_aux =
+        return null
+    }
 }
