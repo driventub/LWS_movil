b6e6a42db101f71312a6876a40c8046f862594f7
driven <aldazpaulo@gmail.com>
2023-08-24 22:01:36 -0500

    boton regresae

--- app/src/main/java/com/aldaz/lws/ui/activities/DatosActivity.kt
+++ app/src/main/java/com/aldaz/lws/ui/activities/DatosActivity.kt
@@ -1,5 +1,6 @@
 package com.aldaz.lws.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
@@ -9,20 +10,24 @@ import com.aldaz.lws.data.connections.ConnectionApi.getService
 import com.aldaz.lws.data.endpoint.Endpoint
 import com.aldaz.lws.data.entitties.lws.DatosItem
 import com.aldaz.lws.databinding.ActivityListaDatosBinding
+import com.aldaz.lws.databinding.DatosBinding
 import com.aldaz.lws.databinding.ItemDatosBinding
 import com.aldaz.lws.ui.viewholder.ExamenesViewModel
 
 
 class DatosActivity : AppCompatActivity() {
 
-    private lateinit var binding: ItemDatosBinding
+//    private lateinit var binding: ItemDatosBinding
+    private lateinit var binding: ActivityListaDatosBinding
     private lateinit var examenesViewModel: ExamenesViewModel
+//    private lateinit var bindingDatos: DatosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ItemDatosBinding.inflate(layoutInflater)
+        binding = ActivityListaDatosBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         buscarExamenPorNumero()
+        regresar()
     }
 
     private fun buscarExamenPorNumero(){
@@ -32,12 +37,20 @@ class DatosActivity : AppCompatActivity() {
         examenesViewModel.datosList.observe(this) { fetchedDatos ->
             // Update your UI with the fetchedDatos here
             // For example, you can set text in TextViews
-            binding.nombreTextView
+
             // ...and so on
         }
 
 
     }
 
+    private fun regresar(){
+        binding.backButton.setOnClickListener() {
+            // Navigate back to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: Close this activity
+        }
+    }
 
 }
--- app/src/main/java/com/aldaz/lws/ui/activities/MainActivity.kt
+++ app/src/main/java/com/aldaz/lws/ui/activities/MainActivity.kt
@@ -1,36 +1,42 @@
-package com.aldaz.lws.ui.activities
+    package com.aldaz.lws.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
-import com.aldaz.lws.databinding.ActivityMainBinding
-import com.aldaz.lws.ui.adapters.LwsAdapter
-import com.aldaz.lws.ui.viewholder.ExamenesViewModel
+    import android.content.Intent
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import androidx.lifecycle.ViewModelProvider
+    import com.aldaz.lws.databinding.ActivityMainBinding
+    import com.aldaz.lws.ui.adapters.LwsAdapter
+    import com.aldaz.lws.ui.viewholder.ExamenesViewModel
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var viewModel: ExamenesViewModel
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        viewModel = ViewModelProvider(this).get(ExamenesViewModel::class.java)
+        private lateinit var binding : ActivityMainBinding
+        private lateinit var viewModel: ExamenesViewModel
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            viewModel = ViewModelProvider(this)[ExamenesViewModel::class.java]
 
-        val adapter = LwsAdapter(emptyList(), { clickedExamen ->
-            // Handle item click
-//            val examenNumero = clickedExamen.numero
-            // Fetch the data using examenNumero and viewModel.fetchDatos(examenNumero)
-        }, viewModel)
+            val adapter = LwsAdapter(emptyList(), { clickedExamen ->
+                val examenNumero = clickedExamen.numero
+                viewModel.fetchDatos(examenNumero) // Fetch the data using examenNumero
 
-        binding.recyclerView.adapter = adapter
-        binding.viewModel = viewModel // Set the ViewModel to the binding
-        binding.lifecycleOwner = this // Set the lifecycle owner for LiveData observation
+                // Navigate to DatosActivity with fetched data
+                val intent = Intent(this, DatosActivity::class.java).apply {
+                    putExtra("EXAMEN_NUMERO", examenNumero)
+                }
+                startActivity(intent)
+            }, viewModel)
 
-        viewModel.examenesList.observe(this) { examenes ->
-            adapter.updateExamenes(examenes)
+            binding.recyclerView.adapter = adapter
+            binding.viewModel = viewModel // Set the ViewModel to the binding
+            binding.lifecycleOwner = this // Set the lifecycle owner for LiveData observation
+
+            viewModel.examenesList.observe(this) { examenes ->
+                adapter.updateExamenes(examenes)
+            }
+
+            viewModel.fetchExamenes()
         }
-
-        viewModel.fetchExamenes()
     }
-}
